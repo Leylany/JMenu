9d4d67ce4f7f3781b4f947468658988dcbf0d509
Leylany <36907439+Leylany@users.noreply.github.com>
2018-04-17 19:56:07 -0500

    Add files via upload

---
+++ UsaMenu.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+class UsaMenu
+{
+  public static void main(String args[])
+  {
+    Menu m = new Menu();
+    m.setSize(400, 300);
+    m.setVisible(true);
+    m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+}
